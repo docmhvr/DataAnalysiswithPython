0e14089181ad7a4a5780b9000dfcdc75ec149440
Mahavir Chandaliya <mahavir37chandaliya@gmail.com>
2024-06-30 02:55:21 +0000

    Implemented boxplot

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
+import numpy as np
 from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
@@ -8,8 +9,7 @@ register_matplotlib_converters()
 df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 
 # Clean data
-df = df[(df['value'] >= df['value'].quantile(0.025)) &
-        (df['value'] <= df['value'].quantile(0.975))]
+df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
 
 def draw_line_plot():
     # Draw line plot
@@ -47,27 +47,25 @@ def draw_bar_plot():
     return fig
 
 def draw_box_plot():
-    # Prepare data for box plots (this part is done!)
     df_box = df.copy()
     df_box.reset_index(inplace=True)
     df_box['year'] = df_box['date'].dt.year
     df_box['month'] = df_box['date'].dt.strftime('%b')
+    df_box['month_num'] = df_box['date'].dt.month
+    df_box = df_box.sort_values('month_num')
 
-    # Draw box plots (using Seaborn)
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(20, 10))
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(15, 5))
 
-    sns.boxplot(x='year', y='value', data=df_box, ax=ax1)
-    ax1.set_title('Year-wise Box Plot (Trend)')
-    ax1.set_xlabel('Year')
-    ax1.set_ylabel('Page Views')
+    sns.boxplot(x='year', y='value', data=df_box, ax=axes[0])
+    sns.boxplot(x='month', y='value', data=df_box, ax=axes[1])
 
-    sns.boxplot(x='month', y='value', data=df_box, ax=ax2, order=[
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-    ])
-    ax2.set_title('Month-wise Box Plot (Seasonality)')
-    ax2.set_xlabel('Month')
-    ax2.set_ylabel('Page Views')
+    axes[0].set_title('Year-wise Box Plot (Trend)')
+    axes[0].set_xlabel('Year')
+    axes[0].set_ylabel('Page Views')
 
-    # Save image and return fig (don't change this part)
+    axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    axes[1].set_xlabel('Month')
+    axes[1].set_ylabel('Page Views')
+
     fig.savefig('box_plot.png')
     return fig
